720bfb06e9354c54915ef4a99aa4579f76a9da32
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-24 13:56:03 +0100

    applied jamespage review fixes

--- hooks/utils.py
+++ hooks/utils.py
@@ -71,10 +71,10 @@ def get_unit_hostname():
 
 @cached
 def get_host_ip(hostname=None):
-    if config('prefer-ipv6'):
-        return get_ipv6_addr()[0]
-
     hostname = hostname or unit_get('private-address')
+    if config('prefer-ipv6'):
+        return hostname or get_ipv6_addr()[0]
+
     try:
         # Test to see if already an IPv4 address
         socket.inet_aton(hostname)
