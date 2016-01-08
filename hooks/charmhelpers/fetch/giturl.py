7a79382774b63443005166dc74c553ee59118268
Chris MacNaughton <chris.macnaughton@canonical.com>
2016-01-08 14:02:43 -0500

    charmhelpers sync

--- hooks/charmhelpers/fetch/giturl.py
+++ hooks/charmhelpers/fetch/giturl.py
@@ -22,7 +22,6 @@ from charmhelpers.fetch import (
     filter_installed_packages,
     apt_install,
 )
-from charmhelpers.core.host import mkdir
 
 if filter_installed_packages(['git']) != []:
     apt_install(['git'])
@@ -50,8 +49,8 @@ class GitUrlFetchHandler(BaseFetchHandler):
             cmd = ['git', '-C', dest, 'pull', source, branch]
         else:
             cmd = ['git', 'clone', source, dest, '--branch', branch]
-        if depth:
-            cmd.extend(['--depth', depth])
+            if depth:
+                cmd.extend(['--depth', depth])
         check_call(cmd)
 
     def install(self, source, branch="master", dest=None, depth=None):
@@ -62,8 +61,6 @@ class GitUrlFetchHandler(BaseFetchHandler):
         else:
             dest_dir = os.path.join(os.environ.get('CHARM_DIR'), "fetched",
                                     branch_name)
-        if not os.path.exists(dest_dir):
-            mkdir(dest_dir, perms=0o755)
         try:
             self.clone(source, dest_dir, branch, depth)
         except OSError as e:
