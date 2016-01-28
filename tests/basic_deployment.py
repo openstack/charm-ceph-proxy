f76fdb91d16d3c226454b9efc74c71a6ac4fc160
Chris MacNaughton <chris.macnaughton@canonical.com>
2016-01-28 10:45:57 +0100

    fix relations

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -26,6 +26,11 @@ class CephBasicDeployment(OpenStackAmuletDeployment):
         self._add_relations()
         self._configure_services()
         self._deploy()
+        
+        u.log.info('Waiting on extended status checks...')
+        exclude_services = ['mysql']
+        self._auto_wait_for_status(exclude_services=exclude_services)
+
         self._initialize_tests()
 
     def _add_services(self):
