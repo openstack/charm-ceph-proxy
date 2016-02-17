80ee80f839c95471da5ca1facfb219ed6c206e99
Liam Young <liam.young@canonical.com>
2016-02-17 08:14:56 +0000

    relation_get needs the remoute unit if not running in the context of the relation that it is updating

--- hooks/ceph_hooks.py
+++ hooks/ceph_hooks.py
@@ -231,7 +231,8 @@ def notify_osds():
 
 def notify_radosgws():
     for relid in relation_ids('radosgw'):
-        radosgw_relation(relid)
+        for unit in related_units(relid):
+            radosgw_relation(relid=relid, unit=unit)
 
 
 def notify_client():
@@ -270,19 +271,20 @@ def osd_relation(relid=None):
 
 @hooks.hook('radosgw-relation-changed')
 @hooks.hook('radosgw-relation-joined')
-def radosgw_relation(relid=None):
+def radosgw_relation(relid=None, unit=None):
     # Install radosgw for admin tools
     apt_install(packages=filter_installed_packages(['radosgw']))
-
+    if not unit:
+        unit=remote_unit()
     """Process broker request(s)."""
     if ceph.is_quorum():
-        settings = relation_get(rid=relid)
+        settings = relation_get(rid=relid, unit=unit)
         if 'broker_req' in settings:
             if not ceph.is_leader():
                 log("Not leader - ignoring broker request", level=DEBUG)
             else:
                 rsp = process_requests(settings['broker_req'])
-                unit_id = remote_unit().replace('/', '-')
+                unit_id = unit.replace('/', '-')
                 unit_response_key = 'broker-rsp-' + unit_id
                 log('mon cluster in quorum - providing radosgw with keys')
                 data = {
